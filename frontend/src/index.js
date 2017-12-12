7d32a6b42f4db30c2c0ed725c4b9bdc8eb9b6bf2
krisztiank <krisztian.korosi@caplin.com>
2017-12-12 14:47:30 +0000

    minor code cleanups

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,18 +14,26 @@ const jenkinsSocket = `ws://${jenkinsDomain}:8081`;
 const app = ReactDOM.render(<App />, document.getElementById("root"));
 
 const refreshData = () => {
-  const request = createRequest("GET", `http://${window.location.hostname}:3030/status.json?view=${VIEW}&jenkinsUrl=${jenkinsUrl}`, response => {
+  const { hash } = window.location;
+  let url = `http://${window.location.hostname}:3030/status.json?jenkinsUrl=${jenkinsUrl}`;
+
+  if (hash.length > 2) {
+    url += `&view=${hash.substring(1)}`;
+  } else {
+    url += `&view=${VIEW}`;
+  }
+  const request = createRequest("GET", url, response => {
     try {
-        response = JSON.parse(response);
-        if (response.jobs) {
-            let { name, url, jobs } = response;
-            if(!ENABLE_GREEN) {
-                jobs = jobs.filter(job => job.color !== 'blue');
-            }
-            app.setState({ name, url, jobs });
+      response = JSON.parse(response);
+      if (response.jobs) {
+        let { name, url, jobs } = response;
+        if (!ENABLE_GREEN) {
+          jobs = jobs.filter(job => job.color !== "blue");
         }
+        app.setState({ name, url, jobs });
+      }
     } catch (e) {
-        console.log("XHR error: ", e);
+      console.log("XHR error: ", e);
     }
   });
 
@@ -41,7 +49,9 @@ setInterval(() => {
 // TODO: this still gives 404, although plugin IS installed...
 const ws = new WebSocket(jenkinsSocket);
 ws.onmessage = function(msg) {
-    console.log("Message received! Yayyy! :)", msg);
+  console.log("Message received! Yayyy! :)", msg);
+    const obj = JSON.parse(msg.data);
+    const { project, result } = obj;
 };
 
 registerServiceWorker();
