aae14a71ab03bda980a1644b695d5be76fc61ff3
krisztiank <krisztian.korosi@caplin.com>
2017-12-13 12:06:30 +0000

    stretched tiles

--- frontend/src/tile/Tile.js
+++ frontend/src/tile/Tile.js
@@ -4,16 +4,18 @@ import "./Tile.less";
 const Tile = ({ name, url, color }) => {
   return (
     <a className={"build-tile " + color} target="_blank" href={url}>
-      {color === "blue" ? (
-        <span className="job-icon" role="img" aria-label="Happy">
-          &#x1F334;
+        <span className="job-texts">
+          {color === "blue" ? (
+            <span className="job-icon" role="img" aria-label="Happy">
+              &#x1F334;
+            </span>
+          ) : (
+            <span className="job-icon" role="img" aria-label="Angry">
+              &#x1F4A5;
+            </span>
+          )}
+          <span className="job-name">{name}</span>
         </span>
-      ) : (
-        <span className="job-icon" role="img" aria-label="Angry">
-          &#x1F4A5;
-        </span>
-      )}
-      {name}
     </a>
   );
 };
