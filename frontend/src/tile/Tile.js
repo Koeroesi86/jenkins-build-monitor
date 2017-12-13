3bd62a7aef17dd70c0dc1e812476830bac2c7e56
krisztiank <krisztian.korosi@caplin.com>
2017-12-13 12:21:10 +0000

    in progress emoji

--- frontend/src/tile/Tile.js
+++ frontend/src/tile/Tile.js
@@ -5,13 +5,24 @@ const Tile = ({ name, url, color }) => {
   return (
     <a className={"build-tile " + color} target="_blank" href={url}>
         <span className="job-texts">
-          {color === "blue" ? (
+          {color === "blue" && (
             <span className="job-icon" role="img" aria-label="Happy">
               &#x1F334;
             </span>
-          ) : (
+          )}
+          {color === "red" && (
             <span className="job-icon" role="img" aria-label="Angry">
               &#x1F4A5;
+            </span>
+          )}
+          {color === "red_anime" && (
+            <span className="job-icon" role="img" aria-label="Angry">
+              &#x23F3;
+            </span>
+          )}
+          {color === "blue_anime" && (
+            <span className="job-icon" role="img" aria-label="Angry">
+              &#x23F3;
             </span>
           )}
           <span className="job-name">{name}</span>
